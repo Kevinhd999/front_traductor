847eff6e71e94cf16f7431006cb8dfd98f7dfe5d
unknown <kmonroyibarra@gmail.com>
2025-03-19 23:06:55 -0500

    Tabs sencillos

--- src/app/principal/principal.component.ts
+++ src/app/principal/principal.component.ts
@@ -1,18 +1,16 @@
-  import { Component, OnInit } from '@angular/core';
-  import { CommonModule } from '@angular/common';
-  import { TutorialComponent } from './tutorial/tutorial.component';
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { TutorialComponent } from './tutorial/tutorial.component';
+import { ManualComponent } from './manual/manual.component';
+import { MenuComponent } from './menu/menu.component';
 
-
-  @Component({
-    selector: 'app-principal',
-    templateUrl: './principal.component.html',
-    styleUrls: ['./principal.component.css'],
-    standalone: true,
-    imports: [CommonModule, TutorialComponent]
-  })
-  export class PrincipalComponent implements OnInit {
-    constructor() {
-     }
-
-    ngOnInit() { }
-  }
+@Component({
+  selector: 'app-principal',
+  templateUrl: './principal.component.html',
+  styleUrls: ['./principal.component.css'],
+  standalone: true,
+  imports: [CommonModule, TutorialComponent, ManualComponent, MenuComponent]
+})
+export class PrincipalComponent {
+  selectedTab: string = 'menu';
+}
